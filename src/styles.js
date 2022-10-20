5783b24b0e3df5a2a101a554ac22c22eaae21c17
ksy <tpdud3256@gmail.com>
2022-10-21 03:12:39 +0900

    Login UI

--- src/styles.js
+++ src/styles.js
@@ -1,13 +1,28 @@
 import { createGlobalStyle } from "styled-components";
 import { reset } from "styled-reset";
 
-export const lightTheme = {};
+export const lightTheme = { fontColor: "#2c2c2c", bgColor: "white" };
 
-export const darkTheme = {};
+export const darkTheme = { fontColor: "white", bgColor: "#2c2c2c" };
 
 //global style - body 전체에 적용
 export const GlobalStyles = createGlobalStyle`
+    ${reset}
+    input {
+        all:unset //input에 적용된 기본 스타일 리셋
+    }
+
+    * { // * 는 전체 요소에 적용됨
+        box-sizing: border-box;
+    }
+
     body {
-        ${reset}
+        background-color: #FAFAFA;
+        font-size: 14px;
+        font-family:'Open Sans', sans-serif;
+    }
+
+    a {
+        text-decoration: none;
     }
 `;
